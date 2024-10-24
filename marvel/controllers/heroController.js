5cac5bc8954a5335b0ad5a19bd2bf54a6d6a0667
Andreas Rahbek Hansen <andreasrh7913@gmail.com>
2024-10-24 10:54:16 +0200

    Routes og controller

--- marvel/controllers/heroController.js
+++ marvel/controllers/heroController.js
@@ -19,5 +19,31 @@ const createHero = (req, res) => {
     res.status(201).json(newHero);
 };
 
+const updateHero = (req, res) => {
+    const heroId = parseInt(req.params.id);
+    const heroIndex = heroes.findIndex(h => h.id === heroId);
+
+    if (heroIndex !== -1) {
+        const updatedHero = { ...heroes[heroIndex], ...req.body };
+        heroes[heroIndex] = updatedHero;
+        res.status(200).json(updatedHero);
+    } else {
+        res.status(404).json({ message: "Hero not found" });
+    }
+};
+
+const deleteHero = (req, res) => {
+    const heroId = parseInt(req.params.id);
+    const heroIndex = heroes.findIndex(h => h.id === heroId);
+
+    if (heroIndex !== -1) {
+        const deletedHero = heroes.splice(heroIndex, 1);
+        res.status(200).json({ message: "Hero deleted", hero: deletedHero[0] });
+    } else {
+        res.status(404).json({ message: "Hero not found" });
+    }
+};
+
+
 // Similarly, implement updateHero and deleteHero
-module.exports = { getHeroes, getHeroById, createHero };
+module.exports = { getHeroes, getHeroById, createHero, updateHero, deleteHero};
--- marvel/routes/heroesRoutes.js
+++ marvel/routes/heroesRoutes.js
@@ -1,62 +1,20 @@
 const express = require('express');
 const router = express.Router();
-let heroesData = require('../heroes.json');
+const heroController = require('../controllers/heroController');
 
 // Get all heroes
-router.get('/heroes', (req, res) => {
-    res.status(200).json(heroesData);
-});
+router.get('/heroes', heroController.getHeroes);
 
 // Get a specific hero by ID
-router.get('/heroes/:id', (req, res) => {
-    const hero = heroesData.find(h => h.id === parseInt(req.params.id));
-    if (hero) {
-        res.status(200).json(hero);
-    } else {
-        res.status(404).json({ message: "Hero not found" });
-    }
-});
+router.get('/heroes/:id', heroController.getHeroById);
 
 // Create a new hero
-router.post('/heroes', (req, res) => {
-    const { id, name, alias, powers } = req.body;
-
-    if (!name || !alias || !powers) {
-        return res.status(400).json({ message: "Name, alias, and powers are required" });
-    }
-
-    const newHero = { id, name, alias, powers };
-    heroesData.push(newHero);
-    res.status(201).json(newHero);
-});
+router.post('/heroes', heroController.createHero);
 
 // Update an existing hero by ID
-router.put('/heroes/:id', (req, res) => {
-    const { name, alias, powers } = req.body;
-    const heroIndex = heroesData.findIndex(h => h.id === parseInt(req.params.id));
-
-    if (heroIndex === -1) {
-        return res.status(404).json({ message: "Hero not found" });
-    }
-
-    if (!name || !alias || !powers) {
-        return res.status(400).json({ message: "Name, alias, and powers are required" });
-    }
-
-    heroesData[heroIndex] = { ...heroesData[heroIndex], name, alias, powers };
-    res.status(200).json(heroesData[heroIndex]);
-});
+router.put('/heroes/:id', heroController.updateHero)
 
 // Delete a hero by ID
-router.delete('/heroes/:id', (req, res) => {
-    const heroIndex = heroesData.findIndex(h => h.id === parseInt(req.params.id));
-
-    if (heroIndex === -1) {
-        return res.status(404).json({ message: "Hero not found" });
-    }
-
-    heroesData.splice(heroIndex, 1);
-    res.status(200).json({ message: "Hero deleted" });
-});
+router.delete('/heroes/:id', heroController.deleteHero)
 
 module.exports = router;
